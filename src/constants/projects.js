5146ff2659760013f3c898ca4109a0b310a1c918
Osama <osama2002ibrahim@gmail.com>
2023-11-13 01:18:37 +0200

    moving to cloudflare

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -4,7 +4,7 @@ const projetcs = [
     description:
       "offline first , full featured quotes generator web app to get some wonderful advice about life, success, money ...etc",
     image: "assets/life.png",
-    liveLink: "https://life-advise.netlify.app/",
+    liveLink: "https://life-advise.pages.dev",
     SourceLink: "https://github.com/osama865/life-advice-client",
   },
   {
@@ -12,15 +12,15 @@ const projetcs = [
     description:
       "Complete Netflix clone with filters to search films and series, Based on ReactJs and firebase to handle database and authentication.",
     image: "assets/netflix.png",
-    liveLink: "https://os-netflix.netlify.app/",
-    SourceLink: "https://os-netflix.netlify.app/",
+    liveLink: "https://os-netflix.pages.dev",
+    SourceLink: "https://o-netflix.pages.dev",
   },
   {
     name: "Restaurant website",
     description:
       "Attractive Restaurant website with all pages that needed by your customers, menu, about, gallary and contact. So they can found everything about your restaurant.",
     image: "assets/restaurant.png",
-    liveLink: "https://o-restaurant.netlify.app/",
+    liveLink: "https://o-restaurant.pages.dev",
     SourceLink: "https://github.com/osama865/assemble-rest",
   },
   {
@@ -28,7 +28,7 @@ const projetcs = [
     description:
       "not Complete amazon clone based on react and firebase and many other",
     image: "assets/amazon.png",
-    liveLink: "https://os-amazon.netlify.app/",
+    liveLink: "https://o-amzn.pages.dev",
     SourceLink: "https://github.com/osama865/Amazon",
   },
   {
@@ -36,7 +36,7 @@ const projetcs = [
     description:
       "My personal Website that contains all my informations and projects, ",
     image: "assets/portoflio.png",
-    liveLink: "https://o-portfolio.netlify.app/",
+    liveLink: "https://o-portfolio.pages.dev",
     SourceLink: "https://github.com/osama865/Projects",
   },
   {
@@ -44,7 +44,7 @@ const projetcs = [
     description:
       "featured hotel rooms web app to look for a rooms to rent for your holiday",
     image: "assets/hotel.png",
-    liveLink: "https://o-hotel.netlify.app/",
+    liveLink: "https://os-hotel.pages.dev",
     SourceLink: "https://github.com/osama865/Hotel",
   },
   {
@@ -52,14 +52,14 @@ const projetcs = [
     description:
       "Funny chat app to chat with your friends and create rooms and joins others rooms based on react and node js and socket.io",
     image: "assets/Chat2.png",
-    liveLink: "https://o-chatting.netlify.app/",
+    liveLink: "https://o-chatting.pages.dev",
     SourceLink: "https://github.com/osama865/Chat-Room-client",
   },
   {
     name: "Galarey",
     description: "simple Galarey app to store your images there and share them",
     image: "assets/firegram.png",
-    liveLink: "https://0firegram.netlify.app/",
+    liveLink: "https://0firegram.pages.dev",
     SourceLink: "https://github.com/osama865/Galarey",
   },
   {
@@ -67,7 +67,7 @@ const projetcs = [
     description:
       "Basic weather app to get the weather status about specific country or city",
     image: "assets/weather.png",
-    liveLink: "https://o-weather.netlify.app/",
+    liveLink: "https://o-weather.pages.dev",
     SourceLink: "https://github.com/osama865/PWA_Weather",
   },
   {
@@ -75,14 +75,14 @@ const projetcs = [
     description:
       "simple app to save food recipes and share it with your friends",
     image: "assets/food.png",
-    liveLink: "https://o-pwa-recipes.netlify.app/",
+    liveLink: "https://o-food.pages.dev",
     SourceLink: "https://github.com/osama865/PWA_FOOD_APP",
   },
   {
     name: "GYM landing page",
     description: "nice landing page for your GYM",
     image: "assets/gym.png",
-    liveLink: "https://max-gym.netlify.app/",
+    liveLink: "https://o-gym.pages.dev",
     SourceLink: "https://github.com/osama865/GYM",
   },
   {
@@ -90,7 +90,7 @@ const projetcs = [
     description:
       "Simple Website For Testing Different Types Of Animations Provided by AOS library, animation-on-scroll",
     image: "assets/aos.png",
-    liveLink: "https://aos-testing.netlify.app/",
+    liveLink: "https://aos-testing.pages.dev",
     SourceLink: "#",
   },
 ];
